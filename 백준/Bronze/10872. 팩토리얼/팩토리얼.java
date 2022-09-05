f6bbd19b1e04e4ade00c2af36627999dc388ba76
zirryo <107678471+zirryo@users.noreply.github.com>
2022-09-05 21:52:19 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+    }
+    static int factorial(int n) {
+        if(n<=1) return 1;
+        return n * factorial(n-1);
+    }
+}
