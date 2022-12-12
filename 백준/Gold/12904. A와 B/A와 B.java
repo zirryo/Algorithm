b14672b12d6cf73bf28c6532e45bc4c11d27372b
Jihyo <107678471+zirryo@users.noreply.github.com>
2022-12-12 20:37:08 +0900

    [Gold V] Title: A와 B, Time: 136 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder s = new StringBuilder(br.readLine());
+        StringBuilder t = new StringBuilder(br.readLine());
+        
+        // t -> s 로 역변환
+        while(s.length() < t.length()) {
+
+            if(t.charAt(t.length()-1) == 'A') {
+                t.deleteCharAt(t.length()-1);
+            } else {
+                t.deleteCharAt(t.length()-1);
+                t.reverse();
+            }
+        }
+
+        if(s.toString().equals(t.toString())) System.out.println(1);
+        else System.out.println(0);
+
+    }
+
+}
