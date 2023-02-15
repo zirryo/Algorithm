b4ac0bb721ca5ec0850a8524cb6545f349053df5
Jihyo <107678471+zirryo@users.noreply.github.com>
2023-02-15 16:17:23 +0900

    [Gold IV] Title: 점프, Time: 200 ms, Memory: 21072 KB -BaekjoonHub

---
+++ 백준/Gold/2253. 점프/점프.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static Set<Integer> smallStones;
+    static final int INF = 10001;
+    static int maxVelocity;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        init();
+        int result = solve();
+        System.out.println(result);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        maxVelocity = (int) Math.pow(2.0*N, 0.5);
+        smallStones = new HashSet<>();
+
+        dp = new int[N+1][maxVelocity+2];
+
+        for(int i=0; i<dp.length; i++) Arrays.fill(dp[i], INF);
+
+        for(int i=0; i<M; i++) {
+            int small = Integer.parseInt(br.readLine());
+            smallStones.add(small);
+        }
+    }
+    private static int solve() {
+        dp[1][0] = 0;
+
+        for(int i=2; i<=N; i++) {
+            if(smallStones.contains(i)) continue;
+            for(int j=1; j<=(int) Math.sqrt(2*i); j++) {
+                dp[i][j] = Math.min(Math.min(dp[i-j][j-1], dp[i-j][j]), dp[i-j][j+1]) + 1;
+            }
+        }
+
+        int result = INF;
+        for(int j=1; j<=maxVelocity; j++) {
+            result = Math.min(dp[N][j], result);
+        }
+
+        if(result == INF) result = -1;
+        return result;
+    }
+    private static class Jump implements Comparable<Jump> {
+        int pos;
+        int jumpSize;
+        int cnt;
+
+        public Jump(int pos, int jumpSize, int cnt) {
+            this.pos = pos;
+            this.jumpSize = jumpSize;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Jump j) {
+            return this.cnt - j.cnt;
+        }
+    }
+}
