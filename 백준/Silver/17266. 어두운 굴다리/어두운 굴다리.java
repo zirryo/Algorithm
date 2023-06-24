6e2dd5b664cfa56ba8310a054657c12c4a83e7e5
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-06-25 00:16:44 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 288 ms, Memory: 24476 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -4,45 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static int N,M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        arr = new int[M];
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int len = Integer.parseInt(st.nextToken());
+        int pre = len;
+        int x = len;
 
-        for(int i = 0; i < M; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i=1; i<K; i++) {
+            x = Integer.parseInt(st.nextToken());
+            int temp = (x - pre + 1) / 2;
+            if (temp > len) len = temp;
+            pre = x;
         }
-
-        int left = 1;
-        int right = N;
-        int result = 0;
-
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            if(possible(mid)) {
-                result = mid;
-                right = mid - 1;
-            }
-            else
-                left = mid + 1;
-        }
-        System.out.println(result);
-    }
-
-    static boolean possible(int target) {
-        int prev = 0;
-
-        for(int i = 0; i < arr.length; i++) {
-
-            if(arr[i] - target <= prev) {
-                prev = arr[i] + target;
-            }
-            else return false;
-        }
-        return N - prev <= 0;
+        len = Math.max(len, N - x);
+        System.out.println(len);
     }
 }
