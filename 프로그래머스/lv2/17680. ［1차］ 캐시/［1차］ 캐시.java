f823b3ea53e1a3e6f569cdb7e1192e2eaf8bc48b
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-07-24 23:44:47 +0900

    [level 2] Title: [1차] 캐시, Time: 2.12 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) return 5 * cities.length;
+        
+        int answer = 0;
+        Queue<String> q = new LinkedList<>();
+        for (String city : cities) {
+            city = city.toLowerCase();
+            if (q.contains(city)) {
+                q.remove(city);
+                q.add(city);
+                answer += 1;
+            } else {
+                if (q.size() >= cacheSize) q.poll();
+                q.add(city);
+                answer += 5;
+            }
+        }
+        return answer;
+    }
+}
