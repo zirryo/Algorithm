c0a49320b4a5d775f973b928ae1bc06859ed94f9
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-07-25 23:58:27 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.43 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public static void main(String[] args) {
+        solution("aa1+aa2", "AAAA12");
+    }
+    public static int solution(String str1, String str2) {
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        Set<String> set = new HashSet<>();
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        for (int i = 1; i < str1.length(); i++) {
+            String s = str1.substring(i-1, i+1);
+            if (Character.isAlphabetic(s.charAt(0)) && Character.isAlphabetic(s.charAt(1))) {
+                map1.put(s, map1.getOrDefault(s, 0) + 1);
+                set.add(s);
+            }
+        }
+        for (int i = 1; i < str2.length(); i++) {
+            String s = str2.substring(i-1, i+1);
+            if (Character.isAlphabetic(s.charAt(0)) && Character.isAlphabetic(s.charAt(1))) {
+                map2.put(s, map2.getOrDefault(s, 0) + 1);
+                set.add(s);
+            }
+        }
+        int union = 0;
+        int intersection = 0;
+        for (String str : set) {
+
+
+            union += Math.max(map1.getOrDefault(str, 0), map2.getOrDefault(str, 0));
+            intersection += Math.min(map1.getOrDefault(str, 0), map2.getOrDefault(str, 0));
+        }
+
+        if (union == 0 && intersection == 0) return 65536;
+        else {
+            return intersection * 65536 / union;
+        }
+    }
+}
