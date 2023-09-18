6fc69cb3cd79ea8ca74c279ae290c9821e9d8ab5
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-09-18 09:25:55 +0900

    [Gold V] Title: 사과나무, Time: 276 ms, Memory: 22896 KB -BaekjoonHub

---
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int div2 = 0;
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            div2 += (arr[i] / 2);
+        }
+
+        if (sum % 3 == 0) {
+            sum /= 3;
+            if (div2 >= sum) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
