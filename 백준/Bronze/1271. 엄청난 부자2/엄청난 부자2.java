f7e91c35b7866fc8526802f9bdae37da64a316c6
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-10-08 23:56:27 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 156 ms, Memory: 16240 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        System.out.print(n.divide(m) + "\n" + n.remainder(m));
+    }
+}
