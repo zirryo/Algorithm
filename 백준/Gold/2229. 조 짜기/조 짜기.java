7cc59d767fd9188de0bedb445770f2ecd8082195
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-10-12 10:23:59 +0900

    [Gold V] Title: 조 짜기, Time: 160 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            score[i] = Integer.parseInt(st.nextToken());
+            int max = -1;
+            int min = 10000;
+
+            for (int j = i; j > 0; j--) {
+                max = Math.max(max, score[j]);
+                min = Math.min(min, score[j]);
+                dp[i] = Math.max(dp[i], max - min + dp[j - 1]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
