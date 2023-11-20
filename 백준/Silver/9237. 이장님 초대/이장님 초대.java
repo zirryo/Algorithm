73d059a1deda67abb6be52f67595dfb6417e669f
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-11-20 23:56:47 +0900

    [Silver V] Title: 이장님 초대, Time: 400 ms, Memory: 28476 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        Arrays.sort(arr);
+        for (int i=N-1; i>=0; i--) {
+            result = Math.max(result, arr[i] + N - i);
+        }
+        System.out.println(result + 1);
+    }
+}
