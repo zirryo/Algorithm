68190c2a668ed6eeebbc0a1dc9d64063037cc014
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2023-12-17 00:57:40 +0900

    [Gold III] Title: 마라톤 2, Time: 236 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Gold/10653. 마라톤 2/마라톤 2.java
+++ 백준/Gold/10653. 마라톤 2/마라톤 2.java
@@ -1,37 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int arr[][],N,K, memo[][];
+    static int N, K;
+    static final int MAX = 500 * 4000;
+    static int[][] cord, dist, dp;
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        int [][]a = new int[N+1][2];
-        for(int i =1; i<= N; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        cord = new int[N+1][2];
+        dist = new int[N+1][N+1];
+        dp = new int[N+1][K+1];
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            a[i][0]= Integer.parseInt(st.nextToken());
-            a[i][1]= Integer.parseInt(st.nextToken());
+            cord[i][0] = Integer.parseInt(st.nextToken());
+            cord[i][1] = Integer.parseInt(st.nextToken());
         }
-        arr = new int[N+1][N+1];
-        for(int i =1; i< N;i++) {
-            for(int j =i+1; j<= N; j++) {
-                arr[i][j] = Math.abs(a[i][0]-a[j][0])+Math.abs(a[i][1]-a[j][1]);
+        for (int i = 1; i < N; i++) {
+            for (int j = i+1; j <= N; j++) {
+                dist[i][j] = Math.abs(cord[i][0] - cord[j][0]) + Math.abs(cord[i][1] - cord[j][1]);
             }
         }
-        memo = new int[N+1][K+1];
-        System.out.println(check(N,K));
+        System.out.println(marathon(N, K));
     }
-    private static int check(int n,int k) {
-        if(memo[n][k]!=0)return memo[n][k];
-        if(n==1) return 0;
-        memo[n][k] = Integer.MAX_VALUE;
-        for(int i = 0; i<= k; i++) {
-            if(0<n-i-1) memo[n][k] = Math.min(check(n-i-1,k-i)+arr[n-i-1][n],memo[n][k] );
+    private static int marathon(int idx, int skip) {
+        if (dp[idx][skip] != 0) return dp[idx][skip];
+        if (idx == 1) return 0; // 1번지점은 출발지
+
+        int temp = MAX;
+        for (int i = 0; i <= skip; i++) {
+            if (idx - i > 1) {
+                temp = Math.min(marathon(idx-i-1, skip-i)
+                        + dist[idx-i-1][idx], temp);
+            }
         }
-        return memo[n][k];
+        return dp[idx][skip] = temp;
     }
 }
