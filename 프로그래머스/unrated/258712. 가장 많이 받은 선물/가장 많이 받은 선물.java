9aaca8090fcc34baf3fd6f90ab4149b4dc2fdee5
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-01-04 20:15:40 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 6.68 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            map.put(friends[i], i);
+        }
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+        
+        for (String str : gifts) {
+            String[] cur = str.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;
+        }
+        
+       int ans = 0;
+       for (int i = 0; i < friends.length; i++) {
+           int cnt = 0;
+           for (int j = 0; j < friends.length; j++) {
+               if(i == j) continue;
+               if (record[i][j] > record[j][i]) cnt++;
+               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
+           }
+           ans = Math.max(cnt, ans);
+       }
+        return ans;
+    }
+}
