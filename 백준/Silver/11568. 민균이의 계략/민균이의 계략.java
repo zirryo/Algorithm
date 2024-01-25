107bb0cdddf21db7466320f6745ee78ab056102d
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-01-25 23:46:25 +0900

    [Silver II] Title: 민균이의 계략, Time: 148 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = N; i > 0; i--) {
+            if (result < dp[i]) result = dp[i];
+        }
+
+        System.out.println(result + 1);
+    }
+}
