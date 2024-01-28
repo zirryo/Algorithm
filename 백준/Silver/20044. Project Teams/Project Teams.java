a23caed390a464765150e13b41d7c0ef6654c00d
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-01-28 19:35:45 +0900

    [Silver IV] Title: Project Teams, Time: 176 ms, Memory: 15968 KB -BaekjoonHub

---
+++ 백준/Silver/20044. Project Teams/Project Teams.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()) * 2;
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int min = 200_000;
+        for (int i = 0; i < N / 2; i++) {
+            min = Math.min(min, arr[i] + arr[N-1-i]);
+        }
+        System.out.println(min);
+    }
+}
