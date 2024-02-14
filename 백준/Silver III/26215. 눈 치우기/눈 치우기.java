ce1c72dfaffcd18ab74ae3495856d2a2804b52d0
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-02-14 23:37:52 +0900

    [Silver III] Title: 눈 치우기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver III/26215. 눈 치우기/눈 치우기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int result = 0;
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            result += b;
+            if (a > b) pq.add(a - b);
+        }
+
+        if (!pq.isEmpty()) result += pq.poll();
+
+        if (result > 1440) System.out.println(-1);
+        else System.out.println(result);
+    }
+}
