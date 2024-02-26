ae0384cadbcf1e21632a2f84b4f5794017d54b0f
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-02-26 19:59:38 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 308 ms, Memory: 24988 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+        long max = 0;
+        long sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            if (i >= M) {
+                sum -= arr[i-M];
+                max = Math.max(max, sum);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
