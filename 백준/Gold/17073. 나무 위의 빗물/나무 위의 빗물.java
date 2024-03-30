999b1fabd9943dce91eb7f0a22dc90486df7a814
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-03-30 12:06:46 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 768 ms, Memory: 180388 KB -BaekjoonHub

---
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        List<Integer>[] graph = new List[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        double leafNodes = 0;
+        for (int i = 2; i <= N; i++) {
+            if (graph[i].size() == 1) leafNodes += 1;
+        }
+
+        System.out.println(W / leafNodes);
+    }
+}
