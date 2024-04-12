173199fb0f57a9cafdceac1f4bf54ba12362565c
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-04-12 14:24:06 +0900

    [Gold V] Title: 시간 관리하기, Time: 144 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] tasks = new int[N][2];
+        StringTokenizer st;
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            tasks[i][0] = Integer.parseInt(st.nextToken());
+            tasks[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(tasks, ((o1, o2) -> o2[1] - o1[1])); // 마감일 내림차순 정렬
+
+        int lastDay = 1_000_001;
+
+        for (int i = 0; i < N; i++) {
+            if (lastDay < tasks[i][1]) {
+                lastDay -= tasks[i][0];
+            } else {
+                lastDay = tasks[i][1] - tasks[i][0];
+            }
+        }
+        if (lastDay <= 0) System.out.println(-1);
+        else System.out.println(lastDay);
+    }
+}
