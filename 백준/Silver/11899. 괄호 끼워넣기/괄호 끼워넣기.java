8326db955276a2d5746e322b0ab6784f668d45a1
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-06-18 23:52:13 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        int open = 0;
+        int close = 0;
+
+        for (char c : arr) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++;
+                }
+            }
+        }
+
+        System.out.println(open + close);
+    }
+}
