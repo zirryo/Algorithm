94779d4531329ac7aa3486d0140694065c1665e1
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-07-26 15:35:02 +0900

    [Silver IV] Title: 스케이트 연습, Time: 152 ms, Memory: 15044 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i =1 ; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        int curSpeed = 0;
+
+        for (int i = N ; i >= 1 ; i--) {
+            curSpeed = Math.min(curSpeed + 1, arr[i]);
+            sum += curSpeed;
+        }
+
+        System.out.println(sum);
+    }
+}
