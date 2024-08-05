a51c1980c48046d943ad0f94f888672b62b4c4f5
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-08-05 19:09:14 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 180 ms, Memory: 18656 KB -BaekjoonHub

---
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        boolean[][] impossible = new boolean[N+1][N+1];
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            impossible[a][b] = true;
+            impossible[b][a] = true;
+        }
+
+        int result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                if (impossible[i][j]) continue;
+                for (int k = j + 1; k <= N; k++) {
+                    if (impossible[i][k] || impossible[k][j]) continue;
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
