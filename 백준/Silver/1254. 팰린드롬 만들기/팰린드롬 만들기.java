959c039172adf19e65ac510cb06c6d7600e310a2
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-11-30 23:50:49 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (isPalindrome(s.substring(i))) {
+                System.out.println(n + i);
+                return;
+            }
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
