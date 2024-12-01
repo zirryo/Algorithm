23285e64de0d6252e40d1f2b1db26afb22ce7f9e
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-12-01 22:54:53 +0900

    [Gold IV] Title: 정복자, Time: 328 ms, Memory: 26892 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int u;
+        int v;
+        int w;
+
+        public Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return w - o.w;
+        }
+    }
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        List<Edge> edges = new ArrayList<>();
+        parent = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(u, v, w));
+        }
+
+        Collections.sort(edges);
+
+        int cost = 0;
+        int edgeCount = 0;
+
+        for (Edge edge : edges) {
+            if (union(edge.u, edge.v)) {
+                cost += edge.w + T * edgeCount;
+                edgeCount++;
+                if (edgeCount == N - 1) break;
+            }
+        }
+
+        System.out.println(cost);
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+            return true;
+        }
+
+        return false;
+    }
+}
