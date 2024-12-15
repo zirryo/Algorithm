668f23cc2047c0d07b72e587e91e7f848e73ac67
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2024-12-15 22:47:22 +0900

    [Silver IV] Title: 피하자, Time: 116 ms, Memory: 14560 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/25379. 피하자/피하자.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                even.add(i);
+            } else {
+                odd.add(i);
+            }
+        }
+
+        int oddToFront = 0;
+        for (int i = 0; i < odd.size(); i++) {
+            oddToFront += Math.abs(odd.get(i) - i);
+        }
+
+        int evenToFront = 0;
+        for (int i = 0; i < even.size(); i++) {
+            evenToFront += Math.abs(even.get(i) - i);
+        }
+
+        System.out.println(Math.min(oddToFront, evenToFront));
+    }
+}
