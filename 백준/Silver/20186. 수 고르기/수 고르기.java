a52d49cab8970651755db0a0e8e4e256622a0804
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-01-12 23:21:07 +0900

    [Silver IV] Title: 수 고르기, Time: 140 ms, Memory: 15704 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/20186. 수 고르기/수 고르기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = n - 1, skip = 0; skip < k; i--, skip++) {
+            sum += arr[i] - skip;
+        }
+
+        System.out.println(sum);
+    }
+}
