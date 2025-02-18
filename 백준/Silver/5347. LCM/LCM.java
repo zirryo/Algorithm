555cf4152bac18a9ba39f65688062d3f290a3796
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-02-18 23:27:40 +0900

    [Silver V] Title: LCM, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            sb.append(lcm(a, b)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+}
