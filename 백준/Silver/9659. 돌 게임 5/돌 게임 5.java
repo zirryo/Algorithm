92f571614c8b12ace02b700cc3e958cebb505d29
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-06-21 21:28:07 +0900

    [Silver III] Title: 돌 게임 5, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println(n % 2 == 1 ? "SK" : "CY");
+    }
+}
