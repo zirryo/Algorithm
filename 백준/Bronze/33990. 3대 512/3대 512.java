b380eb352ab94be9c5781a07c609af5d934a111d
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-08-29 15:09:25 +0900

    [Bronze III] Title: 3대 512, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/33990. 3대 512/3대 512.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            int sum = A + B + C;
+            if (sum >= 512) {
+                answer = Math.min(answer, sum);
+            }
+        }
+
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
