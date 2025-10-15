4422168ef8210df8c4b6cff2a0438f04811adfdb
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-10-15 23:34:41 +0900

    [Bronze III] Title: 수학적 호기심, Time: 680 ms, Memory: 48972 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int count = 0;
+            for (int a = 1; a < n; a++) {
+                for (int b = a + 1; b < n; b++) {
+                    if ((a * a + b * b + m) % (a * b) == 0) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
