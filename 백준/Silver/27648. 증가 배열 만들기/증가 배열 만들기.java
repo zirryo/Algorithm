501187d0975e546724dde1a4f5bad17231b9f6a3
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-11-01 23:36:14 +0900

    [Silver V] Title: 증가 배열 만들기, Time: 460 ms, Memory: 64516 KB -BaekjoonHub

---
+++ 백준/Silver/27648. 증가 배열 만들기/증가 배열 만들기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        if (K < N + M - 1) {
+            System.out.println("NO");
+            return;
+        }
+
+        sb.append("YES\n");
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = (i + j + 1);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
