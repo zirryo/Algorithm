526900bdbbe22f058f2a6549b9e892d69d4bbd49
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2025-12-29 18:11:00 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 100 ms, Memory: 14144 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int LIMIT = 1_000_000_000;
+        System.out.println(LIMIT);
+    }
+
+}
