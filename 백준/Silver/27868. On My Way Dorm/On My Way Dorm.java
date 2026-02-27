976aeb8c178430e0c17e77fe7230b6b5fc37814a
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2026-02-27 12:46:51 +0900

    [Silver III] Title: On My Way Dorm, Time: 292 ms, Memory: 25792 KB -BaekjoonHub

---
+++ 백준/Silver/27868. On My Way Dorm/On My Way Dorm.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        String workCmd = br.readLine();
+        int L = workCmd.length();
+
+        StringBuilder leaveCmd = new StringBuilder(workCmd);
+        leaveCmd.reverse();
+        System.out.println(leaveCmd);
+    }
+}
