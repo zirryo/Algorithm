cf8e2c48656dd8af31a52a92decfae3b1c9576db
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2026-08-03 07:19:50 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] number) {
+        int count = 0;
+        int len = number.length;
+
+        for (int i = 0; i < len - 2; i++) {
+            for (int j = i + 1; j < len - 1; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
