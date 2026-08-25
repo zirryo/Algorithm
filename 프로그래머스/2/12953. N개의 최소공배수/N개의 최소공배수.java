059fbee4db49f8d976a68a0b96a514fc28ff0702
Jihyo Kim <107678471+zirryo@users.noreply.github.com>
2026-08-25 23:35:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+
+        return answer;
+    }
+
+    private int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    private int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
